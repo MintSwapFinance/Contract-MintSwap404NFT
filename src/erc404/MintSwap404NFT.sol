9a68b01d755dd662b33399e874a96fd9878f0c5a
tbag9527 <tbag@mintswap.finance>
2024-07-03 14:19:47 +0800

    whitelist sale

--- src/erc404/MintSwap404NFT.sol
+++ src/erc404/MintSwap404NFT.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "./ERC404.sol";
 import "../metadata/IMetadataRenderer.sol";
 
@@ -10,9 +11,14 @@ contract MintSwap404NFT is ERC404, OwnableUpgradeable, UUPSUpgradeable {
 
     uint256 public constant PUBLIC_SALE_PRICE = 0.04 ether;
 
-    uint256 public constant PUBLIC_SALE_COUNT = 3000;
+    uint256 public constant WHITELIST_SALE_PRICE = 0.028 ether;
+
+    uint256 public constant SALE_TOTAL_COUNT = 3000;
+
+    uint256 public constant WHITELIST_SALE_COUNT = 500;
 
     uint256 public _publicMintedCount;
+    uint256 public _whitelistMintedCount;
 
     struct MintConfig {
         uint32 startTime;
@@ -21,16 +27,27 @@ contract MintSwap404NFT is ERC404, OwnableUpgradeable, UUPSUpgradeable {
 
     MintConfig public mintConfig;
 
+    struct WhitelistConfig {
+        uint32 startTime;
+        uint32 endTime;
+    }
+
+    WhitelistConfig public whitelistConfig;
+
     address public metadataRenderer;
 
     uint256 public constant MINTSWAP_REWARDS_COUNT = 7000;
 
     uint256 public _mintswapMintedCount;
 
+    bytes32 public markleRoot;
+
     error MintNotStart();
     error MintFinished();
 
     event WithdrawETH(address indexed to, uint256 amount);
+
+    error UnauthorizedMinter(address minter);
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
@@ -55,6 +72,12 @@ contract MintSwap404NFT is ERC404, OwnableUpgradeable, UUPSUpgradeable {
         _;
     }
 
+    modifier isWhitslistSaleTime() {
+        if (block.timestamp < whitelistConfig.startTime) revert MintNotStart();
+        if (block.timestamp > whitelistConfig.endTime) revert MintFinished();
+        _;
+    }
+
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
@@ -67,7 +90,7 @@ contract MintSwap404NFT is ERC404, OwnableUpgradeable, UUPSUpgradeable {
     }
 
     function publicSale(uint numberOfTokens) external payable isPublicSaleTime {
-        require(numberOfTokens > 0 && _publicMintedCount + numberOfTokens <= PUBLIC_SALE_COUNT, "Mint numberOfTokens exceeds limit");
+        require(numberOfTokens > 0 && _publicMintedCount + numberOfTokens <= SALE_TOTAL_COUNT, "Mint numberOfTokens exceeds limit");
         require(PUBLIC_SALE_PRICE * numberOfTokens <= msg.value, "Not Enough ETH value to mint tokens");
         
         _mintERC20(_msgSender(), numberOfTokens * units);
@@ -94,6 +117,14 @@ contract MintSwap404NFT is ERC404, OwnableUpgradeable, UUPSUpgradeable {
         mintConfig = MintConfig( _startTime, _endTime);
     }
 
+    function setWhitelistConfig(
+        uint32 _startTime,
+        uint32 _endTime
+    ) external onlyOwner {
+        require(_endTime > _startTime, "MUST(_endTime > _startTime)");
+        whitelistConfig = WhitelistConfig( _startTime, _endTime);
+    }
+
     function _authorizeUpgrade(address newImplementation)
         internal
         onlyOwner
@@ -104,6 +135,30 @@ contract MintSwap404NFT is ERC404, OwnableUpgradeable, UUPSUpgradeable {
         (bool success, ) = _to.call{value: _amount}(new bytes(0));
         require(success, 'ETH transfer failed');
         emit WithdrawETH(_to, _amount);
+    }
+    
+    function whitelistSale(bytes32[] calldata _proof) external payable isWhitslistSaleTime {
+        address account = _msgSender();
+        if (!_verify(account, _proof)) revert UnauthorizedMinter(account);
+        require(_whitelistMintedCount + 1 <= WHITELIST_SALE_COUNT, "White list mint exceeds limit");
+        require(WHITELIST_SALE_PRICE <= msg.value, "Not Enough ETH value to mint tokens");
+        
+        _mintERC20(account, units);
+        _publicMintedCount++;
+        _whitelistMintedCount++;
+    }
+
+    // Merkle verify
+    function _verify(
+        address _account,
+        bytes32[] memory _proof
+    ) internal view returns (bool) {
+        bytes32 leaf = keccak256(abi.encodePacked(_account));
+        return MerkleProof.verify(_proof, markleRoot, leaf);
+    }
+
+    function setMerkleRoot(bytes32 _root) external onlyOwner {
+        markleRoot = _root;
     }
 
 }
