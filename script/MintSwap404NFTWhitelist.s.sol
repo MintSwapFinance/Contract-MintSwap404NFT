aceb82da10094b4ef5c966976f355c673ba773df
tbag9527 <tbag@mintswap.finance>
2024-07-10 15:35:09 +0800

    MS404 whitslist script

---
+++ script/MintSwap404NFTWhitelist.s.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/erc404/MintSwap404NFT.sol";
+
+contract MintSwap404NFTWhitelistScript is Script {
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        address mintswap404NFT = vm.envAddress("MINTSWAP404NFT");
+
+        address[] memory whitelist = vm.envAddress("WHITELIST", ",");
+
+        MintSwap404NFT(mintswap404NFT).addWhitelist(whitelist);
+
+        vm.stopBroadcast();
+    }
+    
+}
